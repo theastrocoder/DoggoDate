b89cd817c9dc6fe93ee1146d10e8a6a5d219dbc1
Simeunovic Žiko <e1428259@student.tuwien.ac.at>
2019-05-14 13:32:39 +0200

    Map Fragment updated to load Google Maps

--- app/src/main/java/io/mse/doggodate/MapFragment.java
+++ app/src/main/java/io/mse/doggodate/MapFragment.java
@@ -8,14 +8,20 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
-    public TextView countTv;
-    public Button countBtn;
+    private GoogleMap mMap;
 
     public MapFragment() {
         // Required empty public constructor
@@ -24,23 +30,22 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.map_fragment, container, false);
-        countTv = (TextView) view.findViewById(R.id.count_tv);
-        countTv.setText("0");
-        countBtn = (Button) view.findViewById(R.id.count_btn);
-        countBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                increaseCount();
-            }
-        });
+        View view = inflater.inflate(R.layout.activity_maps, container, false);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
         return view;
     }
 
-    private void increaseCount() {
-        int current = Integer.parseInt((String) countTv.getText());
-        countTv.setText(String.valueOf(current+1));
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng vienna = new LatLng(48.209585,16.3686083);
+        mMap.addMarker(new MarkerOptions().position(vienna).title("Doggos in Vienna"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(vienna,17));
     }
-
 }
