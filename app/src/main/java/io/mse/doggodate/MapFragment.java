8c00b12e50d5080698e056cdadc5952c0a70826c
Simeunovic Žiko <e1428259@student.tuwien.ac.at>
2019-05-15 18:55:50 +0200

    Slider in map fragment added

--- app/src/main/java/io/mse/doggodate/MapFragment.java
+++ app/src/main/java/io/mse/doggodate/MapFragment.java
@@ -1,11 +1,17 @@
 package io.mse.doggodate;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -13,7 +19,11 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -22,6 +32,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Context context;
 
     public MapFragment() {
         // Required empty public constructor
@@ -30,11 +41,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        context = container.getContext();
         View view = inflater.inflate(R.layout.activity_maps, container, false);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
         return view;
     }
 
@@ -43,9 +57,25 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            context, R.raw.mapstyle));
+
+            if (!success) {
+                Log.e("MapsFragment", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+
+            Log.e("MapsFragment", "Style parsing failed.");
+        }
         // Add a marker in Sydney and move the camera
         LatLng vienna = new LatLng(48.209585,16.3686083);
         mMap.addMarker(new MarkerOptions().position(vienna).title("Doggos in Vienna"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(vienna,17));
+
     }
 }
